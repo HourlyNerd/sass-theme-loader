0968195d77d0eb586e7b76f6f8b0faffbbfa59fd
Misha Koryak <mkoryak@gmail.com>
2016-02-11 23:11:34 -0500

    get the right order

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,6 @@ var _createClass = (function () { function defineProperties(target, props) { for
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 var _sander = require('sander');
@@ -25,52 +23,125 @@ var _glob = require('glob');
 var _glob2 = _interopRequireDefault(_glob);
 
 var STYLE_SOURCE_DIR = '.style-source';
+var CODMAGIC_START = ' ><> codmagic';
+var stylesOutDir = _path2['default'].join('dist', STYLE_SOURCE_DIR);
 
-var resourceOrder = new Set();
+var relativizePath = function relativizePath(outputDir, stylePath) {
+    if (stylePath.match(/node_modules/)) {
+        return _path2['default'].join(stylesOutDir, 'node_modules', stylePath.split('node_modules').pop());
+    } else if (stylePath.match(/(hn-core\/.+)$/)) {
+        return _path2['default'].join(stylesOutDir, 'node_modules', RegExp.$1);
+    } else {
+        return _path2['default'].join(stylesOutDir, _path2['default'].relative(outputDir, stylePath));
+    }
+};
+
+var loader = function loader(source) {
+    this.cacheable && this.cacheable();
+    if (this.resourcePath.match(/\.scss$/)) {
+        return '\n /*!' + CODMAGIC_START + ' ' + relativizePath(this.options.context, this.resourcePath) + ' !*/ \n ' + source;
+    } else {
+        // css files are always included in a scss file. at least I hope they are!
+        // we could make it better and support those other scenarios, but this is an internal tool and I need to sleep
+        return source;
+    }
+};
 
 var StylePackagerPlugin = (function () {
     function StylePackagerPlugin(enabled, outputDir) {
         _classCallCheck(this, StylePackagerPlugin);
 
         this.enabled = enabled;
-        this.outputDir = outputDir;
     }
 
     _createClass(StylePackagerPlugin, [{
         key: 'apply',
         value: function apply(compiler) {
-            var _this = this;
-
             if (!this.enabled) {
                 return;
             }
-            var outputDir = this.outputDir;
+            var outputDir = compiler.context;
+            var records = [];
+            var deps = {};
 
+            // BOOM! trying finding this knowledge bomb in the webpack docs!
+            compiler.options.module.preLoaders.push({
+                test: /\.s?css$/,
+                include: function include() {
+                    return true;
+                },
+                loader: "sass-theme"
+            });
+
+            compiler.plugin('done', function (stats) {
+
+                ///////////////////////// begin hacky cod /////////////////////
+                // TODO: cheating at 11pm. we can figure out this filename from some object we have access to in here!
+                // no really, this needs to be fixed before it breaks
+                var cssFilename = 'css/app-' + stats.toJson().hash + '.min.css';
+                // TODO: well, i started writing hacky cod, might as well continue!!
+                var cssFileBody = _sander2['default'].readFileSync(_path2['default'].join(outputDir, 'dist', cssFilename), { encoding: 'utf8' });
+                /////////////////////// end hacky cod ////////////////////////
+
+                var order = [];
+                cssFileBody.replace(RegExp('/\\*!' + CODMAGIC_START + ' ([^\\s]+?) !\\*/', 'gm'), function (poop, res) {
+                    order.push(res);
+                });
+                records = _lodash2['default'].sortBy(_lodash2['default'].uniqBy(records, 'resource'), function (it) {
+                    var idx = order.indexOf(it.resource);
+                    if (idx >= 0) {
+                        return idx;
+                    } else {
+                        return order.length;
+                    }
+                }).filter(function (it) {
+                    //css resources which were not in the order list will get included since they are deps of the scss
+                    return !(order.indexOf(it.resource) < 0 && it.resource.match(/\.css$/));
+                });
+
+                var getPackagePath = function getPackagePath(stylePath) {
+                    var parts = _path2['default'].dirname(stylePath).split('node_modules');
+                    var packagename = parts.pop().match(/([^\/]+)/)[1];
+                    return _path2['default'].join(parts.join('node_modules'), 'node_modules', packagename);
+                };
+
+                _sander2['default'].rimrafSync(_path2['default'].join(outputDir, stylesOutDir));
+                Object.keys(deps).forEach(function (stylePath) {
+                    if (stylePath.match(/\.css$/)) {
+                        (function () {
+                            // webpack is annoying and doesnt tell me about deps of css files, so i must copy the whole package
+                            var packagePath = getPackagePath(stylePath);
+                            var searchGlob = _path2['default'].join(packagePath, "/**/*.+(png|gif|jpeg|jpg|ttf|eot|svg|otf|woff|woff2)");
+                            var dependsList = _glob2['default'].sync(searchGlob);
+                            dependsList.push(stylePath);
+                            dependsList.forEach(function (from) {
+                                var ending = _path2['default'].relative(packagePath, from);
+                                if (ending.match(/\/(?:node_modules|examples?|tests?|docs?|jekyll)\//)) {
+                                    return; //dont copy assets out of package's node_modules, they are unlikely to be needed
+                                }
+                                var to = _path2['default'].join(getPackagePath(deps[stylePath]), _path2['default'].relative(packagePath, from));
+                                _sander2['default'].copyFileSync(from, { encoding: 'utf-8' }).to(_path2['default'].join(outputDir, to), { encoding: 'utf-8' });
+                            });
+                        })();
+                    } else {
+                        _sander2['default'].copyFileSync(stylePath, { encoding: 'utf-8' }).to(_path2['default'].join(outputDir, deps[stylePath]), { encoding: 'utf-8' });
+                    }
+                });
+                _sander2['default'].writeFileSync(_path2['default'].join(outputDir, stylesOutDir, 'records.json'), JSON.stringify(records, null, 4));
+            });
             compiler.plugin('compilation', function (compilation) {
                 compilation.plugin('after-optimize-tree', function (chunks, modules) {
-                    var stylesOutDir = _path2['default'].join('dist', STYLE_SOURCE_DIR);
-                    var deps = {};
+
                     var isStyle = function isStyle(fn) {
                         return fn && fn.match(/\.s?css/i);
                     };
                     var isScript = function isScript(fn) {
                         return fn && fn.match(/\.js/i);
                     };
-                    var records = [];
-
-                    var relativizePath = function relativizePath(stylePath) {
-                        if (stylePath.match(/node_modules/)) {
-                            return _path2['default'].join(stylesOutDir, 'node_modules', stylePath.split('node_modules').pop());
-                        } else if (stylePath.match(/(hn-core\/.+)$/)) {
-                            return _path2['default'].join(stylesOutDir, 'node_modules', RegExp.$1);
-                        } else {
-                            return _path2['default'].join(stylesOutDir, _path2['default'].relative(outputDir, stylePath));
-                        }
-                    };
 
                     var recordStylePath = function recordStylePath(fn, isDependency) {
                         if (isDependency && !isScript(fn) || isStyle(fn)) {
-                            return deps[fn] = relativizePath(fn);
+                            return deps[fn] = relativizePath(outputDir, fn);
                         }
                     };
                     modules.forEach(function (m) {
@@ -93,56 +164,6 @@ var StylePackagerPlugin = (function () {
                             });
                         }
                     });
-                    var order = [].concat(_toConsumableArray(resourceOrder)).map(function (it) {
-                        return relativizePath(it);
-                    });
-                    records = _lodash2['default'].sortBy(_lodash2['default'].uniqBy(records, 'resource'), function (it) {
-                        var idx = order.indexOf(it.resource);
-                        if (it.resource.match(/\/_?vendor\.scss$/)) {
-                            //resourceOrder isnt actually gauaranteed to be any good. this is a shitty hack
-                            return -2;
-                        } else if (idx >= 0) {
-                            return idx;
-                        } else if (it.resource.match(/\.css$/)) {
-                            return -1;
-                        } else {
-                            return order.length;
-                        }
-                    }).map(function (it) {
-                        //css resources which were not in the order list will get included since they are deps of the scss
-                        it.exclude = !!(order.indexOf(it.resource) < 0 && it.resource.match(/\.css$/));
-                        return it;
-                    });
-
-                    var getPackagePath = function getPackagePath(stylePath) {
-                        var parts = _path2['default'].dirname(stylePath).split('node_modules');
-                        var packagename = parts.pop().match(/([^\/]+)/)[1];
-                        return _path2['default'].join(parts.join('node_modules'), 'node_modules', packagename);
-                    };
-
-                    _sander2['default'].rimrafSync(_path2['default'].join(outputDir, stylesOutDir));
-                    Object.keys(deps).forEach(function (stylePath) {
-                        if (stylePath.match(/\.css$/)) {
-                            (function () {
-                                // webpack is annoying and doesnt tell me about deps of css files, so i must copy the whole package
-                                var packagePath = getPackagePath(stylePath);
-                                var searchGlob = _path2['default'].join(packagePath, "/**/*.+(png|gif|jpeg|jpg|ttf|eot|svg|otf|woff|woff2)");
-                                var dependsList = _glob2['default'].sync(searchGlob);
-                                dependsList.push(stylePath);
-                                dependsList.forEach(function (from) {
-                                    var ending = _path2['default'].relative(packagePath, from);
-                                    if (ending.match(/\/(?:node_modules|examples?|tests?|docs?|jekyll)\//)) {
-                                        return; //dont copy assets out of package's node_modules, they are unlikely to be needed
-                                    }
-                                    var to = _path2['default'].join(getPackagePath(deps[stylePath]), _path2['default'].relative(packagePath, from));
-                                    _sander2['default'].copyFileSync(from).to(_path2['default'].join(_this.outputDir, to));
-                                });
-                            })();
-                        } else {
-                            _sander2['default'].copyFileSync(stylePath).to(_path2['default'].join(_this.outputDir, deps[stylePath]));
-                        }
-                    });
-                    _sander2['default'].writeFileSync(_path2['default'].join(outputDir, stylesOutDir, 'records.json'), JSON.stringify(records, null, 4));
                 });
             });
         }
@@ -151,11 +172,6 @@ var StylePackagerPlugin = (function () {
     return StylePackagerPlugin;
 })();
 
-module.exports = function (content) {
-    this.cacheable && this.cacheable();
-    resourceOrder.add(this.resourcePath);
-    return content;
-};
-
+module.exports = loader;
 module.exports.plugin = StylePackagerPlugin;
 module.exports.STYLE_SOURCE_DIR = STYLE_SOURCE_DIR;
--- src/index.js
+++ src/index.js
@@ -3,43 +3,118 @@ import path from 'path';
 import _ from 'lodash';
 import glob from 'glob';
 
-const STYLE_SOURCE_DIR = '.style-source'
+const STYLE_SOURCE_DIR = '.style-source';
+const CODMAGIC_START = ' ><> codmagic';
+const stylesOutDir = path.join('dist', STYLE_SOURCE_DIR);
 
-const resourceOrder = new Set();
+const relativizePath = (outputDir, stylePath) => {
+    if(stylePath.match(/node_modules/)){
+        return path.join(stylesOutDir, 'node_modules', stylePath.split('node_modules').pop());
+    } else if(stylePath.match(/(hn-core\/.+)$/)){
+        return path.join(stylesOutDir, 'node_modules', RegExp.$1);
+    } else {
+        return path.join(stylesOutDir, path.relative(outputDir, stylePath));
+    }
+};
+
+
+const loader = function(source) {
+    this.cacheable && this.cacheable();
+    if(this.resourcePath.match(/\.scss$/)){
+        return `\n /*!${CODMAGIC_START} ${relativizePath(this.options.context, this.resourcePath)} !*/ \n ${source}`;
+    } else {
+        // css files are always included in a scss file. at least I hope they are!
+        // we could make it better and support those other scenarios, but this is an internal tool and I need to sleep
+        return source;
+    }
+};
 
 class StylePackagerPlugin {
     constructor(enabled, outputDir){
         this.enabled = enabled;
-        this.outputDir = outputDir;
     }
 
     apply(compiler) {
         if(!this.enabled){
             return;
         }
-        const outputDir = this.outputDir;
+        const outputDir = compiler.context;
+        let records = [];
+        const deps = {};
 
+        // BOOM! trying finding this knowledge bomb in the webpack docs!
+        compiler.options.module.preLoaders.push({
+            test: /\.s?css$/,
+            include: () => true,
+            loader: "sass-theme"
+        })
+
+
+        compiler.plugin('done', (stats) => {
+
+
+            ///////////////////////// begin hacky cod /////////////////////
+            // TODO: cheating at 11pm. we can figure out this filename from some object we have access to in here!
+            // no really, this needs to be fixed before it breaks
+            const cssFilename = `css/app-${stats.toJson().hash}.min.css`
+            // TODO: well, i started writing hacky cod, might as well continue!!
+            const cssFileBody = sander.readFileSync(path.join(outputDir, 'dist', cssFilename), {encoding:'utf8'});
+            /////////////////////// end hacky cod ////////////////////////
+
+
+            const order = [];
+            cssFileBody.replace(RegExp(`/\\*!${CODMAGIC_START} ([^\\s]+?) !\\*/`, 'gm'), (poop, res) => {
+                order.push(res);
+            });
+            records = _.sortBy(_.uniqBy(records, 'resource'), (it) => {
+                const idx = order.indexOf(it.resource);
+                if (idx >= 0) {
+                    return idx;
+                } else {
+                    return order.length;
+                }
+            }).filter(it => {
+                //css resources which were not in the order list will get included since they are deps of the scss
+                return !(order.indexOf(it.resource) < 0 && it.resource.match(/\.css$/));
+            });
+
+            const getPackagePath = (stylePath) => {
+                const parts = path.dirname(stylePath).split('node_modules');
+                const packagename = parts.pop().match(/([^\/]+)/)[1];
+                return path.join(parts.join('node_modules'), 'node_modules', packagename);
+            };
+
+            sander.rimrafSync(path.join(outputDir, stylesOutDir));
+            Object.keys(deps).forEach((stylePath) => {
+                if(stylePath.match(/\.css$/)){
+                    // webpack is annoying and doesnt tell me about deps of css files, so i must copy the whole package
+                    const packagePath = getPackagePath(stylePath);
+                    const searchGlob = path.join(packagePath, "/**/*.+(png|gif|jpeg|jpg|ttf|eot|svg|otf|woff|woff2)");
+                    const dependsList = glob.sync(searchGlob);
+                    dependsList.push(stylePath);
+                    dependsList.forEach((from) => {
+                        const ending = path.relative(packagePath, from);
+                        if(ending.match(/\/(?:node_modules|examples?|tests?|docs?|jekyll)\//)){
+                            return; //dont copy assets out of package's node_modules, they are unlikely to be needed
+                        }
+                        const to = path.join(getPackagePath(deps[stylePath]), path.relative(packagePath, from));
+                        sander.copyFileSync(from, {encoding: 'utf-8'}).to(path.join(outputDir, to), {encoding: 'utf-8'});
+                    });
+                } else {
+                    sander.copyFileSync(stylePath, {encoding: 'utf-8'}).to(path.join(outputDir, deps[stylePath]), {encoding: 'utf-8'});
+                }
+            });
+            sander.writeFileSync(path.join(outputDir, stylesOutDir, 'records.json'), JSON.stringify(records, null, 4));
+        })
         compiler.plugin('compilation', (compilation) => {
             compilation.plugin('after-optimize-tree' , (chunks, modules) => {
-                const stylesOutDir = path.join('dist', STYLE_SOURCE_DIR);
-                const deps = {};
+
                 const isStyle = fn => fn && fn.match(/\.s?css/i);
                 const isScript = fn => fn && fn.match(/\.js/i);
-                let records = [];
-
-                const relativizePath = (stylePath) => {
-                    if(stylePath.match(/node_modules/)){
-                        return path.join(stylesOutDir, 'node_modules', stylePath.split('node_modules').pop());
-                    } else if(stylePath.match(/(hn-core\/.+)$/)){
-                        return path.join(stylesOutDir, 'node_modules', RegExp.$1);
-                    } else {
-                        return path.join(stylesOutDir, path.relative(outputDir, stylePath));
-                    }
-                };
 
                 const recordStylePath = (fn, isDependency) => {
                     if((isDependency && !isScript(fn)) || isStyle(fn)){
-                        return deps[fn] = relativizePath(fn);
+                        return deps[fn] = relativizePath(outputDir, fn);
                     }
                 };
                 modules.forEach((m) => {
@@ -60,62 +135,12 @@ class StylePackagerPlugin {
                         });
                     }
                 });
-                const order = [...resourceOrder].map(it => relativizePath(it))
-                records = _.sortBy(_.uniqBy(records, 'resource'), (it) => {
-                    const idx = order.indexOf(it.resource);
-                    if (it.resource.match(/\/_?vendor\.scss$/)) {
-                        //resourceOrder isnt actually gauaranteed to be any good. this is a shitty hack
-                        return -2;
-                    } else if (idx >= 0) {
-                        return idx;
-                    } else if (it.resource.match(/\.css$/)) {
-                        return -1;
-                    } else {
-                        return order.length;
-                    }
-                }).map(it => {
-                    //css resources which were not in the order list will get included since they are deps of the scss
-                    it.exclude = !!(order.indexOf(it.resource) < 0 && it.resource.match(/\.css$/));
-                    return it;
-                });
-                
-                const getPackagePath = (stylePath) => {
-                    const parts = path.dirname(stylePath).split('node_modules');
-                    const packagename = parts.pop().match(/([^\/]+)/)[1];
-                    return path.join(parts.join('node_modules'), 'node_modules', packagename);
-                };
 
-                sander.rimrafSync(path.join(outputDir, stylesOutDir));
-                Object.keys(deps).forEach((stylePath) => {
-                    if(stylePath.match(/\.css$/)){
-                        // webpack is annoying and doesnt tell me about deps of css files, so i must copy the whole package
-                        const packagePath = getPackagePath(stylePath);
-                        const searchGlob = path.join(packagePath, "/**/*.+(png|gif|jpeg|jpg|ttf|eot|svg|otf|woff|woff2)");
-                        const dependsList = glob.sync(searchGlob);
-                        dependsList.push(stylePath);
-                        dependsList.forEach((from) => {
-                            const ending = path.relative(packagePath, from);
-                            if(ending.match(/\/(?:node_modules|examples?|tests?|docs?|jekyll)\//)){
-                                return; //dont copy assets out of package's node_modules, they are unlikely to be needed
-                            }
-                            const to = path.join(getPackagePath(deps[stylePath]), path.relative(packagePath, from));
-                            sander.copyFileSync(from).to(path.join(this.outputDir, to));
-                        });
-                    } else {
-                        sander.copyFileSync(stylePath).to(path.join(this.outputDir, deps[stylePath]));
-                    }
-                });
-                sander.writeFileSync(path.join(outputDir, stylesOutDir, 'records.json'), JSON.stringify(records, null, 4));
             });
         });
     }
 }
 
-module.exports = function (content) {
-    this.cacheable && this.cacheable();
-    resourceOrder.add(this.resourcePath);
-    return content;
-};
-
+module.exports = loader;
 module.exports.plugin = StylePackagerPlugin;
 module.exports.STYLE_SOURCE_DIR = STYLE_SOURCE_DIR;
